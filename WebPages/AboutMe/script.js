c9e0d166c6ac5250fe65252eb37508fbde5cf47f
Nothinormuch <nothin.main@outlook.com>
2026-02-15 00:43:15 +0530

    fix: animation on both navmenu open and close

---
+++ WebPages/AboutMe/script.js
@@ -0,0 +1,21 @@
+let navmenu = document.querySelector(".navmenu");
+let navmenuDivs = document.querySelectorAll(".navmenu div")
+let hamCheckboxLabel = document.querySelector(".ham-checkbox-label");
+let hamCheckbox = document.querySelector(".ham-checkbox");
+
+navmenu.classList.add('navmenuUp');
+
+hamCheckboxLabel.addEventListener('click',hamClick);
+
+function hamClick(ev) {
+    if(hamCheckbox.checked) {
+        console.log("ham: MenuClose");
+        navmenu.classList.remove('navmenuDown');
+        navmenu.classList.add('navmenuUp');
+    }
+    else {
+        console.log("ham: MenuOpen");
+        navmenu.classList.remove('navmenuUp');
+        navmenu.classList.add('navmenuDown');
+    }
+}
